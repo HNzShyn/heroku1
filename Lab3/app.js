3b74a12e9d5ccf68334e2e1e34e324d9a7d2bfe7
HNzShyn <nvu1343@gmail.com>
2022-09-24 13:40:38 +0700

    ok

---
+++ Lab3/app.js
@@ -0,0 +1,11 @@
+const http = require("http");
+//Note: Muốn deploy lên hosting(Heroku) thì cần phải set port với process.env.PORT || 3000.
+const port = process.env.PORT || 3000;
+//Request: client -> server
+//Response: server -> client
+const app  = http.createServer((req, res) =>{
+    res.statusCode = 200;   //HTTP_OK
+    res.setHeader("Content-Type", "text/html");
+    res.end("Hello Word");
+
+})
