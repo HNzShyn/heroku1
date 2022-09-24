facb091ab7c6532a5b0c232c6d272a0985af133a
HNzShyn <nvu1343@gmail.com>
2022-09-24 13:43:30 +0700

    ok

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,6 +1,9 @@
+//Bước 1: Khai báo http modul
 const http = require("http");
 //Note: Muốn deploy lên hosting(Heroku) thì cần phải set port với process.env.PORT || 3000.
+//Bước 2: Khai báo port của server
 const port = process.env.PORT || 3000;
+//Bước 3: Tạo web server, setheader + code
 //Request: client -> server
 //Response: server -> client
 const app  = http.createServer((req, res) =>{
@@ -8,7 +11,7 @@ const app  = http.createServer((req, res) =>{
     res.setHeader("Content-Type", "text/html");
     res.end("Hello Word");
 });
-
+//Bước 4: Chạy web bằng cách listen port
 app.listen(port);
 
 
